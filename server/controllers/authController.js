281ed7f8403b4cbe576c1bd81038d1116ea46a8b
tupsi07 <sanjida657@yahoo.com>
2025-06-14 18:09:07 +0600

    checkauth route and controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -221,8 +221,17 @@ const updateUser = async(req, res) =>{
 
 }
 
+const checkAuth = async(req, res)=>{
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message:'Internal server error'})
+    }
+}
+
 export {
-    forgotPassword, loginUser,
+    checkAuth, forgotPassword, loginUser,
     registerUser, resetPassword, updateUser
 };
 
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { forgotPassword, loginUser, registerUser, resetPassword, updateUser } from '../controllers/authController.js'
+import { checkAuth, forgotPassword, loginUser, registerUser, resetPassword, updateUser } from '../controllers/authController.js'
 import protectRoute from '../middleware/authentication/protectRoute.js'
 const authRoute = express.Router()
 
@@ -8,6 +8,7 @@ authRoute.post('/register', registerUser)
 authRoute.post('/forgotPassward', forgotPassword)
 authRoute.patch('/resetPassword/:token', resetPassword)
 authRoute.patch('/updateUser',protectRoute, updateUser)
+authRoute.get('/checkAuth',protectRoute, checkAuth)
 
 export default authRoute
 
