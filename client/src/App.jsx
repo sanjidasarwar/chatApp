13aa546b93c95fa2c536ab697f310f1a83aac394
tupsi07 <sanjida657@yahoo.com>
2025-06-16 10:10:35 +0600

    use authUser in front end route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "./App.css";
+import { AuthContext } from "./context/authContext";
 import Chat from "./pages/Chat/Chat";
 import Login from "./pages/Login/Login";
 import NotFound from "./pages/NotFound/NotFound";
 import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
-
 function App() {
+  const { authUser } = useContext(AuthContext);
   return (
     <>
       <ToastContainer />
       <Routes>
-        <Route index element={<Login />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/profile-update" element={<ProfileUpdate />} />
+        <Route
+          index
+          element={!authUser ? <Login /> : <Navigate to="/chat" />}
+        />
+        <Route
+          path="/chat"
+          element={authUser ? <Chat /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile-update"
+          element={authUser ? <ProfileUpdate /> : <Navigate to="/" />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
