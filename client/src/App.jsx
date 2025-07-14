1b88649a6016f01712b0daacb3a9d8a99c537c30
tupsi07 <sanjida657@yahoo.com>
2025-07-14 15:10:29 +0600

    add authUser to route so that user cant go to login throug url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "./App.css";
+import { AuthContext } from "./context/AuthContext";
 import Chat from "./pages/Chat/Chat";
 import Login from "./pages/Login/Login";
 import NotFound from "./pages/NotFound/NotFound";
 import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
 function App() {
+  const { authUser } = useContext(AuthContext);
   return (
     <>
       <ToastContainer autoClose={1000} />
       <Routes>
-        <Route index element={<Login />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/profile-update" element={<ProfileUpdate />} />
+        <Route index element={!authUser ? <Login /> : <Chat />} />
+        <Route
+          path="/chat"
+          element={authUser ? <Chat /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile-update"
+          element={authUser ? <ProfileUpdate /> : <Navigate to="/" />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
